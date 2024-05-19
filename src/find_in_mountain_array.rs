38166f3742601ecbed95bd6865344d8ca7788aac
Alexander Egorov <egoroff@gmail.com>
2024-05-19 16:18:20 +0300

    refactor: find_in_mountain_array task refactored

--- src/find_in_mountain_array.rs
+++ src/find_in_mountain_array.rs
@@ -4,33 +4,31 @@ impl Solution {
     pub fn find_in_mountain_array(target: i32, mountain_arr: &MountainArray) -> i32 {
         let mut low = 0usize;
         let mut high = mountain_arr.length() as usize - 1;
+        let mut size = high;
         while high - low > 1 {
-            let mid = low + ((high - low) / 2);
+            let mid = low + size / 2;
             let v = mountain_arr.get(mid as i32);
             if v < mountain_arr.get(mid as i32 + 1) && v > mountain_arr.get(mid as i32 - 1) {
                 low = mid;
             } else {
                 high = mid;
             }
+            size = high - low;
         }
         let mount = high;
 
-        if mountain_arr.get(mount as i32) == target {
-            return mount as i32;
-        }
-
         low = 0usize;
         high = mount;
+        size = high;
         while high - low > 1 {
-            let mid = low + ((high - low) / 2);
+            let mid = low + size / 2;
             let v = mountain_arr.get(mid as i32);
-            if target < v {
-                high = mid;
-            } else if v == target {
-                return mid as i32;
-            } else {
-                low = mid;
+            match target.cmp(&v) {
+                std::cmp::Ordering::Less => high = mid,
+                std::cmp::Ordering::Equal => return mid as i32,
+                std::cmp::Ordering::Greater => low = mid,
             }
+            size = high - low;
         }
 
         if mountain_arr.get(low as i32) == target {
@@ -43,16 +41,16 @@ impl Solution {
 
         low = mount;
         high = mountain_arr.length() as usize - 1;
+        size = high - low;
         while high - low > 1 {
-            let mid = low + ((high - low) / 2);
+            let mid = low + size / 2;
             let v = mountain_arr.get(mid as i32);
-            if target < v {
-                low = mid;
-            } else if v == target {
-                return mid as i32;
-            } else {
-                high = mid;
+            match target.cmp(&v) {
+                std::cmp::Ordering::Less => low = mid,
+                std::cmp::Ordering::Equal => return mid as i32,
+                std::cmp::Ordering::Greater => high = mid,
             }
+            size = high - low;
         }
 
         if mountain_arr.get(low as i32) == target {
@@ -149,5 +147,19 @@ mod test {
         let arr = vec![1, 2, 3, 5, 3];
         let arr = MountainArray::new(arr);
         assert_eq!(Solution::find_in_mountain_array(1, &arr), 0);
+    }
+
+    #[test]
+    fn test10() {
+        let arr = vec![
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+            25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+            47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+            69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+            91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91,
+            90, 89, 88, 87, 86, 85, 84, 83, 82,
+        ];
+        let arr = MountainArray::new(arr);
+        assert_eq!(Solution::find_in_mountain_array(102, &arr), -1);
     }
 }
