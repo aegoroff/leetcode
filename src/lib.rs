c31930bca8e4b575fdfd5c74969cd1f78dce7aa9
Alexander Egorov <egoroff@gmail.com>
2024-05-27 20:55:47 +0300

    feat: Number of Provinces task

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+pub mod add_two_nodes;
 pub mod all_ancestors;
 pub mod find_if_path_exists_in_grapsh;
 pub mod find_in_mountain_array;
 pub mod house_robbery;
 pub mod house_robbery2;
+pub mod longest_consecutive;
 pub mod longest_increasing_subseq;
 pub mod min_sub_array_len;
+pub mod num_of_provinces;
 pub mod palindrome_partition;
-pub mod longest_consecutive;
-pub mod add_two_nodes;
---
+++ src/num_of_provinces.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+        let mut graph: HashMap<i32, Vec<i32>> = HashMap::with_capacity(is_connected.len());
+        (0..is_connected.len()).for_each(|i| {
+            for j in 0..is_connected[i].len() {
+                if i == j || is_connected[i][j] == 0 {
+                    continue;
+                }
+                graph
+                    .entry(i as i32)
+                    .and_modify(|e| e.push(j as i32))
+                    .or_insert(vec![j as i32]);
+            }
+        });
+        let mut visited: Vec<i32> = vec![-1; is_connected.len()];
+        let mut color = 0;
+        for v in 0..is_connected.len() {
+            if visited[v] == -1 {
+                color += 1;
+                dfs(&graph, &mut visited, v as i32, color);
+            }
+        }
+        color
+    }
+}
+
+fn dfs(graph: &HashMap<i32, Vec<i32>>, visited: &mut Vec<i32>, v: i32, color: i32) {
+    visited[v as usize] = color;
+    if let Some(adj) = graph.get(&v) {
+        for u in adj {
+            let to_color = visited[*u as usize];
+            if to_color == -1 {
+                dfs(graph, visited, *u, color);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::num_of_provinces::Solution;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            Solution::find_circle_num(vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]]),
+            2
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            Solution::find_circle_num(vec![vec![1, 0, 0], vec![0, 1, 0], vec![0, 0, 1]]),
+            3
+        );
+    }
+}
